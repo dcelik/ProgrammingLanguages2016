c87f2ab674966533adbe479fd0d90000f05a3c85
Deniz Celik <deniz.celik@students.olin.edu>
2016-12-11 01:32:54 -0500

    MOD: working on converting stuff into C

--- Final Project/test_asm.c
+++ Final Project/test_asm.c
@@ -1,20 +1,21 @@
-unsigned long pc = 0;
-unsigned long addr=0;
-
-int env[256] = {0};
-int args[2] = {0};
-
-typedef struct{
-    int addr;
+typedef struct tClosure{
+    int val;
+    void* addr;
+    int isInt;
     int envLen;
-    int *p_env;
-} tTuple;
+    struct tClosure *p_env;
+} tClosure;
 
-tTuple result;
-int fresh_arg = 0;
-int fresh_env = 0;
-int i=0;
-tTuple t[2];
+void* addr;
+
+//RESULT
+tClosure result;
+//ARGS
+tClosure args[2];
+int args_index = 0;
+//ENV
+tClosure *env;
+int env_index = 0;
 
 //int n = 10;
 
@@ -27,10 +28,10 @@ int oper_minus(int arr[]){
 int oper_times(int arr[]){
     return arr[0]*arr[1];
 }
-int oper_zero(int arr[]){
+int oper_zero(tClosure clo[]){
     //true->1
     //false->0
-    return arr[0]==0;
+    return clo[0].val==0;
 }
 
 //-2 -> plus
@@ -44,52 +45,132 @@ int code[59] = {2,10,3,2,0,3,9,8,10,8,7,1,4,1,3,
     0,5,6,4,2,0};
 
 int main(){
-    result.addr = -1;
+    //Setup result
+    result.val = -1;
+    result.isInt = 0;
     result.envLen = 0;
-    result.p_env = (int *)malloc(sizeof(int)*result.envLen);
+    result.p_env = (tClosure *)malloc(sizeof(tClosure)*result.envLen);
+    env = (tClosure *)malloc(sizeof(tClosure)*0);
+
+    //LOAD 10
+    result.val = 10;
+    result.isInt = 1;
+
+    //PUSH-ARGS
+    args[args_index] = result;
+    args_index++;
+
+    //LOAD 0
+    result.val = 0;
+    result.isInt = 1;
+
+    //PUSH-ARGS
+    args[args_index] = result;
+    args_index++;
+
+    //LOAD-ADDR @loop
+    addr = &&LOOP;
+
+    //#loop
+    LOOP:
+
+    //LOAD-FUN
+    result.addr = addr;
+    result.envLen = sizeof(env)/sizeof(tClosure);
+    result.p_env = env;
+
+    //PUSH-ENV
+    tClosure* temp_env = realloc(env,sizeof(env)+sizeof(result));
+    env = temp_env;
+    env[env_index]=result;
+    env_index++;
+
+    //PUSH-ENV-ARGS
+    temp_env = realloc(env,sizeof(env)+sizeof(args));
+    env = temp_env;
+    if(args_index>=1){
+        env[env_index]=args[0];
+        env_index++;
+    }
+    if(args_index==2){
+        env[env_index]=args[1];
+        env_index++;
+    }
+
+    //CLEAR-ARGS
+    args_index = 0;
+
+    //LOOKUP 1
+    result = env[1];
+
+    //PUSH-ARGS
+    args[args_index] = result;
+    args_index++;
+
+    //PRIM-CALL
+    result.val = oper_zero(args);
+    result.isInt = 1;
+
+    //return env[0].val;
+
+
+
+//    goto RES;
+//
+//    DONE:
+//    goto RES;
+//    return result.addr;
+//
+//    RES:
+//
+//    result.addr = -1;
+//    result.envLen = 0;
+//    result.p_env = (int *)malloc(sizeof(int)*result.envLen);
+//
+//    goto DONE;
 
     //tTuple test={-1,10};
     //t[1] = test;
     //t[2] = test;
     //return t[1].addr+t[2].addr;
 
+//
+//    //LOAD\n 10
+//    asm("movl $10, _result\n");
+//
+//    //PUSH-ARGS\n
+//    asm("movl _fresh_arg, %eax\n"
+//        "movl _result, %edx\n"
+//        "movl %edx, _args(,%eax,4)\n");
+//
+//    //fresh_arg++;
+//    asm("movl _fresh_arg, %eax\n"
+//        "addl $1, %eax\n"
+//        "movl %eax, _fresh_arg\n");
+//
+//    //LOAD\n 0
+//    asm("movl $0, _result\n");
+//
+//    //PUSH-ARGS\n
+//    asm("movl _fresh_arg, %eax\n"
+//        "movl _result, %edx\n"
+//        "movl %edx, _args(,%eax,4)\n");
+//
+//    //fresh_arg++;
+//    asm("movl _fresh_arg, %eax\n"
+//        "addl $1, %eax\n"
+//        "movl %eax, _fresh_arg\n");
+//
+//    //@loop
+//    asm("LOOP:\n");
 
-    //LOAD\n 10
-    asm("movl $10, _result\n");
 
-    //PUSH-ARGS\n
-    asm("movl _fresh_arg, %eax\n"
-        "movl _result, %edx\n"
-        "movl %edx, _args(,%eax,4)\n");
-
-    //fresh_arg++;
-    asm("movl _fresh_arg, %eax\n"
-        "addl $1, %eax\n"
-        "movl %eax, _fresh_arg\n");
-
-    //LOAD\n 0
-    asm("movl $0, _result\n");
-
-    //PUSH-ARGS\n
-    asm("movl _fresh_arg, %eax\n"
-        "movl _result, %edx\n"
-        "movl %edx, _args(,%eax,4)\n");
-
-    //fresh_arg++;
-    asm("movl _fresh_arg, %eax\n"
-        "addl $1, %eax\n"
-        "movl %eax, _fresh_arg\n");
-
-    //@loop
-    asm("LOOP:\n");
-
-
-    asm("movl _args, %eax\n"
-        "leave\n"
-        "ret\n");
+    //asm("movl _args, %eax\n"
+    //    "leave\n"
+    //    "ret\n");
     //return args[0];
 
-//    result = n; //LOAD 0-1
+//    result = 10; //LOAD 0-1
 //
 //    args[fresh_arg] = result; //PUSH-ARGS 2
 //    fresh_arg++; //args = [n]
@@ -121,8 +202,6 @@ int main(){
 //
 //    //LOAD-ADDR
 //    addr = 55;
-//
-//    if (result == true){}
 
 
 
@@ -137,15 +216,17 @@ int main(){
 
     //set args to index value
 //    asm("movl $0, _i\n"
-//        "   jmp D2\n"
-//        "D3:\n"
+//        "   jmp pl_8\n"
+//        "pl_55:\n"
 //        "   movl _i, %eax\n"
 //        "   movl _i, %edx\n"
 //        "   movl %edx, _args(,%eax,4)\n"
 //        "   addl $1, _i\n"
-//        "D2:\n"
+//        "pl_8:\n"
 //        "   cmpl $127, _i\n"
-//        "   jle D3\n");
+//        "   jle pl_55\n");
+//
+//    return args[100];
 
     //clear args
 //    asm("movl $0, _i\n"
