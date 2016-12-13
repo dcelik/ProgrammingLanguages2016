2ee37d648522b37a88f622eeec80a615735ac6a9
Jacob Riedel <jr112394@gmail.com>
2016-12-13 18:10:36 -0500

    MOD: Commented out auto-run function

--- Final Project/codecompiler4.py
+++ Final Project/codecompiler4.py
@@ -1594,4 +1594,4 @@ start = ["#start0","PUSH-ENV-ARGS","CLEAR-ARGS","LOOKUP",0,"PUSH-ARGS","LOOKUP",
                 "PUSH-ENV-ARGS","CLEAR-ARGS","LOOKUP",0,"PUSH-ARGS","LOOKUP",1,"PUSH-ARGS","PRIM-CALL",oper_minus,"CLEAR-ARGS","PUSH-ARGS","LOOKUP",2,"LOAD-ADDR-ENV","JUMP","#start32",
                 "PUSH-ENV-ARGS","CLEAR-ARGS","LOOKUP",0,"PUSH-ARGS","PRIM-CALL",oper_zero,"CLEAR-ARGS","PUSH-ARGS","LOOKUP",1,"LOAD-ADDR-ENV","JUMP","#start45",
                 "PUSH-ENV-ARGS","LOOKUP",0,"RETURN","#START_FUNC"]    
-assemble_c(start+code)
+# assemble_c(start+code)
